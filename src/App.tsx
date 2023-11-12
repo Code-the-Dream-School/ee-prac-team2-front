c528b2c7f6ec724336f02465dab0b3160df25bc2
catbus00 <catmousekill@gmail.com>
2023-11-12 16:46:02 +0700

    Create built-in activity list and create activities route

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import Logo from "@assets/logo.png";
+import ActivityList from "@components/Activities/ActivitiesList";
 import DataContainer from "@components/DataContainer/DataContainer";
 import HelloWorld from "@components/HelloWorld/HelloWorld";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 import styles from "./App.module.css";
 
@@ -8,13 +10,25 @@ export default function App() {
   return (
     <main className={styles.main}>
       <img className={styles.logo} alt="React logo" width="400px" src={Logo} />
-      <DataContainer />
-      <div>
-        {`VITE_SOME_ENVIRONMENT_VARIABLE from .env.local: "${
-          import.meta.env.VITE_SOME_ENVIRONMENT_VARIABLE
-        }"`}
-      </div>
-      <HelloWorld msg="Hello EE Practicum Team 2!" />
+      <BrowserRouter>
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <div>
+                <div>
+                  {`VITE_SOME_ENVIRONMENT_VARIABLE from .env.local: "${
+                    import.meta.env.VITE_SOME_ENVIRONMENT_VARIABLE
+                  }"`}
+                </div>
+                <HelloWorld msg="Hello EE Practicum Team 2!" />
+                <DataContainer />
+              </div>
+            }
+          />
+          <Route path="/activities" element={<ActivityList />} />
+        </Routes>
+      </BrowserRouter>
     </main>
   );
 }
---
+++ src/components/Activities/Definition.tsx
@@ -0,0 +1,9 @@
+interface Activities {
+  id: number;
+  name: string;
+  description: string;
+  voteCount: number;
+  category: string;
+}
+
+export type { Activities };
