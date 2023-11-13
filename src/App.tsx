8c571a770acf8cf0ca38cc58c73113f04fd8ecbf
anniethewebcoder <annie.the.web.coder@gmail.com>
2023-11-12 23:26:18 -0600

    list and map

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,24 @@
-import Logo from "@assets/logo.png";
-import DataContainer from "@components/DataContainer/DataContainer";
-import HelloWorld from "@components/HelloWorld/HelloWorld";
+// import Logo from "@assets/logo.png";
+// import DataContainer from "@components/DataContainer/DataContainer";
+// import HelloWorld from "@components/HelloWorld/HelloWorld";
 
-import styles from "./App.module.css";
+//import styles from "./App.module.css";
+
+import Activities from "@components/Activities/Activities";
 
 export default function App() {
   return (
-    <main className={styles.main}>
-      <img className={styles.logo} alt="React logo" width="400px" src={Logo} />
-      <DataContainer />
-      <div>
-        {`VITE_SOME_ENVIRONMENT_VARIABLE from .env.local: "${
-          import.meta.env.VITE_SOME_ENVIRONMENT_VARIABLE
-        }"`}
-      </div>
-      <HelloWorld msg="Hello EE Practicum Team 2!" />
-    </main>
+    // <main className={styles.main}>
+    //   <img className={styles.logo} alt="React logo" width="400px" src={Logo} />
+    //   <DataContainer />
+    //   <div>
+    //     {`VITE_SOME_ENVIRONMENT_VARIABLE from .env.local: "${
+    //       import.meta.env.VITE_SOME_ENVIRONMENT_VARIABLE
+    //     }"`}
+    //   </div>
+    //   <HelloWorld msg="Hello EE Practicum Team 2!" />
+    // </main>
+
+    <Activities />
   );
 }
