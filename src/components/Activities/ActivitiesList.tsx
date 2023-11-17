3a021684e7c360a699f86182b17138498344c140
anniethewebcoder <annie.the.web.coder@gmail.com>
2023-11-17 09:09:34 -0600

    useState to load a list

--- src/components/Activities/ActivitiesList.tsx
+++ src/components/Activities/ActivitiesList.tsx
@@ -1,5 +1,6 @@
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-ignore
+import { useState, useEffect } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faThumbsUp, faThumbsDown } from "@fortawesome/free-regular-svg-icons";
 import styles from "./ActivitiesList.module.css";
@@ -9,10 +10,14 @@ interface ActivitiesListProps {
 }
 
 export default function ActivitiesList(props: ActivitiesListProps) {
+  const [activitiesList, setActivitiesList] = useState(props);
+
+  console.log(activitiesList);
+
   return (
     <div className={styles.list__main}>
       <h1 className={styles.list__h1}>Your Activities</h1>
-      {props.list.map((item: any) => (
+      {activitiesList.list.map((item: any) => (
         <div key={item.id} className={styles.list__card}>
           <h2>Activity: {item.name}</h2>
           <p>Description: {item.description}</p>
