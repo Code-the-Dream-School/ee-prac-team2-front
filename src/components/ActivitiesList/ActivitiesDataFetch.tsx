c3fda11570c15ab73f8ae40d850cda63a82a8476
catbus00 <catmousekill@gmail.com>
2023-11-27 09:01:03 +0700

    Create ActivitiesDataFetch.tsx file to fulfill the ticket  Display activities from backend#17

---
+++ src/components/ActivitiesList/ActivitiesDataFetch.tsx
@@ -0,0 +1,52 @@
+import { Activity } from "@components/ActivitiesList/ActivitiesList";
+import axios from "axios";
+import React, { ReactElement, useEffect, useState } from "react";
+
+import styles from "./ActivitiesList.module.css";
+
+const URL = "http://localhost:8000/api/v1/activities";
+
+const ActivitiesList: React.FC = (): ReactElement => {
+  const [activities, setActivities] = useState<Activity[]>([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const {
+          data: { activities },
+        } = await axios.get(URL);
+        setActivities(activities);
+      } catch (error) {
+        console.error("Error fetching activities:", error);
+        if (axios.isAxiosError(error)) {
+          return {
+            data: "API request failed! Did you remember to `npm run dev` the backend app?",
+          };
+        }
+        return {
+          data: "A fetching data error occurred.",
+        };
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  return (
+    <div className={styles.activitiesContainer}>
+      <h1>Activities</h1>
+      <div className={styles.activitiesList}>
+        {activities.map((activity) => (
+          <div key={activity._id} className={styles.activityItem}>
+            <p>Activity Name: {activity.name}</p>
+            <p>Description: {activity.description}</p>
+            <p>Category: {activity.category}</p>
+            <p>Vote Count: {activity.votes}</p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default ActivitiesList;
