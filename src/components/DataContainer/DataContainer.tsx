f0f5b4adbf5ec44e58af0342fd5de95856951d6d
anniethewebcoder <annie.the.web.coder@gmail.com>
2023-12-05 12:22:01 -0500

    change URL in datacontainer for backend

--- src/components/DataContainer/DataContainer.tsx
+++ src/components/DataContainer/DataContainer.tsx
@@ -1,7 +1,8 @@
 import { getAllData } from "@utils";
 import React, { ReactElement, useEffect, useState } from "react";
 
-const URL = "http://localhost:8000/api/v1/";
+//const URL = "http://localhost:8000/api/v1/";
+const URL = import.meta.env.VITE_BACKEND_URL;
 
 export default function DataContainer(): ReactElement {
   const [message, setMessage] = useState("");
