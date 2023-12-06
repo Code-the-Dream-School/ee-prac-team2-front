8a6cb0141bfdb42987d0e2706e399fe6b6ff6528
catbus00 <catmousekill@gmail.com>
2023-12-06 15:57:01 +0700

    update server proxy port

--- vite.config.ts
+++ vite.config.ts
@@ -18,7 +18,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:8080",
+        target: "http://localhost:8000",
         secure: false,
         rewrite: (path) => path.replace(/^\/api/, ""),
       },
