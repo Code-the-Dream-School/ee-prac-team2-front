6c8e56b083769f81c2d0fdb8110129cf57d4aeb0
Victoria240 <taiwovictoria024@gmail.com>
2023-12-12 01:30:18 +0000

    made a request to users api

--- src/components/Group/CreateGroupForm.tsx
+++ src/components/Group/CreateGroupForm.tsx
@@ -1,24 +1,77 @@
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-nocheck
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 const CreateGroupForm = ({ onCreateGroup }) => {
   const [groupName, setGroupName] = useState("");
   const [description, setDescription] = useState("");
   const [category, setCategory] = useState("");
+  const [selectedUsers, setSelectedUsers] = useState([]);
+  const [users, setUsers] = useState([]);
+  const [loadingUsers, setLoadingUsers] = useState(true);
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
+  useEffect(() => {
+    const fetchUsers = async () => {
+      try {
+        const response = await fetch(
+          "https://dn-live-test.onrender.com/api/v1/users"
+        );
 
-    // Dummy data for API call (replace with actual API call)
-    const groupData = {
-      groupName,
-      description,
-      category,
+        if (response.ok) {
+          const { users } = await response.json();
+          setUsers(users);
+        } else {
+          console.error("Failed to fetch users:", response.statusText);
+        }
+      } catch (error) {
+        console.error("Error fetching registered users:", error);
+      } finally {
+        setLoadingUsers(false);
+      }
     };
 
-    // Call the parent component's callback to handle group creation
-    onCreateGroup(groupData);
+    fetchUsers();
+  }, []);
+
+  const handleCheckboxChange = (userId) => {
+    const updatedUsers = selectedUsers.includes(userId)
+      ? selectedUsers.filter((id) => id !== userId)
+      : [...selectedUsers, userId];
+
+    setSelectedUsers(updatedUsers);
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const response = await fetch(
+        "https://dn-live-test.onrender.com/api/v1/groups",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            groupName,
+            description,
+            category,
+            groupMembers: selectedUsers,
+          }),
+        }
+      );
+
+      if (response.ok) {
+        const { newGroupId } = await response.json();
+        // Handle success, maybe redirect to the new group page
+        console.log("Group created successfully:", newGroupId);
+      } else {
+        // Handle error
+        console.error("Group creation failed.");
+      }
+    } catch (error) {
+      console.error("Error during group creation:", error);
+    }
   };
 
   return (
@@ -52,6 +105,28 @@ const CreateGroupForm = ({ onCreateGroup }) => {
           onChange={(e) => setCategory(e.target.value)}
         />
       </label>
+      <div />
+      <div>
+        <h4>Select Members:</h4>
+        {loadingUsers ? (
+          <p>Loading users...</p>
+        ) : (
+          <div>
+            {users.map((user) => (
+              <div key={user._id}>
+                <input
+                  type="checkbox"
+                  id={user._id}
+                  value={user._id}
+                  checked={selectedUsers.includes(user._id)}
+                  onChange={() => handleCheckboxChange(user._id)}
+                />
+                <label htmlFor={user._id}>{user.name}</label>
+              </div>
+            ))}
+          </div>
+        )}
+      </div>
       <div />
       <button type="submit">Create Group</button>
     </form>
