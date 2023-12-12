6bd6453cac1c17a6812ae731740da91557cae4c5
Annie <annie.the.web.coder@gmail.com>
2023-12-12 03:11:46 -0500

    Activities (#32)
    * list and map
    
    * moved list to App.tsx and created interface
    
    * css module
    
    * react router
    
    * fontawesome icons
    
    * useState to load a list
    
    * added more info to the list
    
    * deleted extra info on mock data
    
    * got rid of extra info
    
    * added materials ui
    
    * Uninstall unused fortAwesome packages
    
    * regenerate package-lock
    
    * deleted css module and eslint auto update
    
    * regenerate package lock
    
    * p tag error fixed
    
    ---------
    
    Co-authored-by: Darga Darga <github@dargacode.com>

--- src/App.tsx
+++ src/App.tsx
@@ -2,33 +2,37 @@ import Logo from "@assets/logo.png";
 import DataContainer from "@components/DataContainer/DataContainer";
 import HelloWorld from "@components/HelloWorld/HelloWorld";
 import ActivitiesListContainer from "@containers/ActivitiesContainer";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
 
 import styles from "./App.module.css";
 
 export default function App() {
   return (
-    <main className={styles.main}>
-      <img className={styles.logo} alt="React logo" width="400px" src={Logo} />
-      <BrowserRouter>
-        <Routes>
-          <Route
-            path="/"
-            element={
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <main className={styles.main}>
+              <img
+                className={styles.logo}
+                alt="React logo"
+                width="400px"
+                src={Logo}
+              />
+              <DataContainer />
               <div>
-                <div>
-                  {`VITE_SOME_ENVIRONMENT_VARIABLE from .env.local: "${
-                    import.meta.env.VITE_SOME_ENVIRONMENT_VARIABLE
-                  }"`}
-                </div>
-                <HelloWorld msg="Hello EE Practicum Team 2!" />
-                <DataContainer />
+                {`VITE_SOME_ENVIRONMENT_VARIABLE from .env.local: "${
+                  import.meta.env.VITE_SOME_ENVIRONMENT_VARIABLE
+                }"`}
               </div>
-            }
-          />
-          <Route path="/activities" Component={ActivitiesListContainer} />{" "}
-        </Routes>
-      </BrowserRouter>
-    </main>
+              <HelloWorld msg="Hello EE Practicum Team 2!" />
+              <Link to="/activities">Link to Activities</Link>
+            </main>
+          }
+        ></Route>
+        <Route path="/activities" Component={ActivitiesListContainer} />
+      </Routes>
+    </BrowserRouter>
   );
 }
--- src/components/ActivitiesList/mocks.ts
+++ src/components/ActivitiesList/mocks.ts
@@ -3,80 +3,6 @@ import { Activity } from "@components/ActivitiesList/ActivitiesList";
 export const ACTIVITIES_MOCK: Activity[] = [
   {
     id: 1,
-    name: "Kayaking",
-    description: "Relax on a nice sunny day with a trip down the river.",
-    voteCount: 4,
-    category: "outdoor",
-  },
-
-  {
-    id: 2,
-    name: "Movies",
-    description:
-      "If the weather is bad outside, enjoy a cozy movie in a movie theater.",
-    voteCount: 2,
-    category: "indoor",
-  },
-  {
-    id: 3,
-    name: "Fondue",
-    description:
-      "If you enjoy cooking, try making fondue as a romantic activity.",
-    voteCount: 2,
-    category: "indoor",
-  },
-  {
-    id: 4,
-    name: "Hiking",
-    description: "An outdoor activity your friends will enjoy.",
-    voteCount: 6,
-    category: "outdoor",
-  },
-  {
-    id: 5,
-    name: "Salsa Class",
-    description:
-      "Learn a new dance and meet new people by attending a salsa class.",
-    voteCount: 4,
-    category: "indoor",
-  },
-  {
-    id: 6,
-    name: "Hiking",
-    description: "Enjoy a day in nature.",
-    voteCount: 0,
-    category: "Outdoor",
-  },
-  {
-    id: 7,
-    name: "Cooking Class",
-    description: "Learn to cook delicious dishes.",
-    voteCount: 0,
-    category: "Indoor",
-  },
-  {
-    id: 8,
-    name: "Movie Night",
-    description: "Watch a favorite movie together.",
-    voteCount: 0,
-    category: "Indoor",
-  },
-  {
-    id: 9,
-    name: "Beach Picnic",
-    description: "Relax by the seaside with a picnic.",
-    voteCount: 0,
-    category: "Outdoor",
-  },
-  {
-    id: 10,
-    name: "Board Games Night",
-    description: "Play fun board games with friends.",
-    voteCount: 0,
-    category: "Indoor",
-  },
-  {
-    id: 11,
     name: "Death on the Nile",
     description:
       "Watch a movie starring Kenneth Branaugh about solving a murder mystery.",
@@ -84,7 +10,7 @@ export const ACTIVITIES_MOCK: Activity[] = [
     category: "Indoor",
   },
   {
-    id: 12,
+    id: 2,
     name: "Make Pasta",
     description:
       "Make pasta from scratch using flour and egg. Then cook and use favorite sauce.",
@@ -92,21 +18,21 @@ export const ACTIVITIES_MOCK: Activity[] = [
     category: "Indoor",
   },
   {
-    id: 13,
+    id: 3,
     name: "Swim 10 laps",
     description: "Swim 10 laps in the pool.",
     voteCount: 0,
     category: "Indoor/Outdoor",
   },
   {
-    id: 14,
+    id: 4,
     name: "Wedding Crasher",
     description: "Go to a random wedding and crash",
     voteCount: 4,
     category: "Indoor",
   },
   {
-    id: 15,
+    id: 5,
     name: "Netflix and Relax",
     description:
       "Pick a movie on Netflix and relax. No discration. You can also discuss the movie like pretentious intellectuals.",
@@ -114,17 +40,91 @@ export const ACTIVITIES_MOCK: Activity[] = [
     category: "Indoor",
   },
   {
-    id: 16,
+    id: 6,
     name: "Play musical instruments",
     description: "Learn an instrument of choice and jam together.",
     voteCount: 3,
     category: "Indoor",
   },
   {
-    id: 17,
+    id: 7,
     name: "Batting Cage Therapy",
     description: "Go to a batting cage and hit your problems away.",
     voteCount: 2,
     category: "Outdoor",
+  },
+  {
+    id: 8,
+    name: "Kayaking",
+    description: "Relax on a nice sunny day with a trip down the river.",
+    voteCount: 4,
+    category: "outdoor",
+  },
+
+  {
+    id: 9,
+    name: "Movies",
+    description:
+      "If the weather is bad outside, enjoy a cozy movie in a movie theater.",
+    voteCount: 2,
+    category: "indoor",
+  },
+  {
+    id: 10,
+    name: "Fondue",
+    description:
+      "If you enjoy cooking, try making fondue as a romantic activity.",
+    voteCount: 2,
+    category: "indoor",
+  },
+  {
+    id: 11,
+    name: "Hiking",
+    description: "An outdoor activity your friends will enjoy.",
+    voteCount: 6,
+    category: "outdoor",
+  },
+  {
+    id: 12,
+    name: "Salsa Class",
+    description:
+      "Learn a new dance and meet new people by attending a salsa class.",
+    voteCount: 4,
+    category: "indoor",
+  },
+  {
+    id: 13,
+    name: "Hiking",
+    description: "Enjoy a day in nature.",
+    voteCount: 0,
+    category: "Outdoor",
+  },
+  {
+    id: 14,
+    name: "Cooking Class",
+    description: "Learn to cook delicious dishes.",
+    voteCount: 0,
+    category: "Indoor",
+  },
+  {
+    id: 15,
+    name: "Movie Night",
+    description: "Watch a favorite movie together.",
+    voteCount: 0,
+    category: "Indoor",
+  },
+  {
+    id: 16,
+    name: "Beach Picnic",
+    description: "Relax by the seaside with a picnic.",
+    voteCount: 0,
+    category: "Outdoor",
+  },
+  {
+    id: 17,
+    name: "Board Games Night",
+    description: "Play fun board games with friends.",
+    voteCount: 0,
+    category: "Indoor",
   },
 ];
