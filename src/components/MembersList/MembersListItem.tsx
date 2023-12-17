c1a5391540b5ef8bad1b795d879adc310c84d5c4
Darga Darga <github@dargacode.com>
2023-12-17 11:29:27 -0700

    add stubs

---
+++ src/components/MembersList/MembersListItem.tsx
@@ -0,0 +1,11 @@
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-nocheck
+
+export default function MembersListItem() {
+  return (
+    <div>
+      <h5>MembersListItem</h5>
+      <p>User summary</p>
+    </div>
+  );
+}
---
+++ src/components/VotableActivitiesList/VotableActivitiesListItem.tsx
@@ -0,0 +1,11 @@
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-nocheck
+
+export default function VotableActivitiesListItem() {
+  return (
+    <div>
+      <h5>VotableActivitiesListItem</h5>
+      <p>Activity which allows voting</p>
+    </div>
+  );
+}
