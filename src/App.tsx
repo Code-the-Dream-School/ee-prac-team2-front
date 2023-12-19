55f9adf558b374e16092f9755cfdb03654791492
Darga Darga <github@dargacode.com>
2023-12-18 20:27:19 -0700

    fix routes
    * consistently use `Component` instead of `Element`
    
    * re-resolve merge conflict which was reverted
    
    * order alphabetically

--- src/App.tsx
+++ src/App.tsx
@@ -6,34 +6,25 @@ import GroupCreationContainer from "@components/Group/GroupCreationContainer";
 import Footer from "@components/HomePage/Footer";
 import HomePage from "@components/HomePage/HomePage";
 import NavBar from "@components/HomePage/NavBar";
-import ViewSingleEventContainer from "@components/ViewSingleEvent/ViewSingleEventContainer";
-import ViewSingleGroup from "@components/ViewSingleGroup/ViewSingleGroup";
 import ActivitiesListContainer from "@containers/ActivitiesContainer";
 import EventContainer from "@containers/EventContainer";
 import GroupContainer from "@containers/GroupContainer";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-
-import styles from "./App.module.css";
 
 const App = () => {
   return (
     <BrowserRouter>
       <NavBar />
       <Routes>
-        <Route path="/" element={<HomePage />} />
+        <Route path="/" Component={HomePage} />
+        <Route path="/about" Component={AboutPage} />
+        <Route path="/account" Component={AuthenticatedContent} />
         <Route path="/activities" Component={ActivitiesListContainer} />
-        <Route path="/signup" element={<AccountCreationContainer />} />
-        <Route path="/about" element={<AboutPage />} />
-        <Route path="/account" element={<AuthenticatedContent />} />
-        <Route path="/groups/:id" element={<ViewSingleGroup />} />
-        <Route path="/events/:id" element={<ViewSingleEventContainer />} />
-        {/*TODO needs real path with group id */}
-        <Route path="/group" Component={GroupContainer} />
+        <Route path="/create-group" Component={GroupCreationContainer} />
         <Route path="/events" Component={AddEventToGroupForm} />
-        {/* TODO needs real path with group and event ids */}
-        <Route path="/event" Component={EventContainer} />
-        {/* New route for group creation */}
-        <Route path="/create-group" element={<GroupCreationContainer />} />
+        <Route path="/events/:id" Component={EventContainer} />
+        <Route path="/groups/:id" Component={GroupContainer} />
+        <Route path="/signup" Component={AccountCreationContainer} />
       </Routes>
       <Footer />
     </BrowserRouter>
