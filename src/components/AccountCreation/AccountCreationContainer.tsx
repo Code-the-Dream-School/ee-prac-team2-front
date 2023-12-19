4f71b99512a57e999f07b447415dc5fbeaab1b70
Darga Darga <github@dargacode.com>
2023-12-18 22:27:40 -0700

    remove unused import

--- src/components/AccountCreation/AccountCreationContainer.tsx
+++ src/components/AccountCreation/AccountCreationContainer.tsx
@@ -2,7 +2,6 @@
 // @ts-nocheck
 
 import Login from "@components/Login/Login";
-import axios from "axios";
 import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 
